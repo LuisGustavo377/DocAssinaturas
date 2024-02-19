bf6a401316fb984ca869886317ca2b8a1cd6664b
LuisGustavo377 <luuis.gustavo.andrade7@gmail.com>
2024-02-19 15:42:55 -0300

    fix: buscar pessoas

--- application/public/assets/js/buscarPessoa.js
+++ application/public/assets/js/buscarPessoa.js
@@ -1,27 +1,25 @@
 
-$(document).ready(function() {
-    $('#pessoaInput').on('input', function() {
-        var cpf = $(this).val();
-        if (cpf.length === 11) { // Assuming CPF has 11 digits
-            $.ajax({
-                url: '/api/pessoafisica/' + cpf,
-                type: 'GET',
-                success: function(response) {
-                    if (response) {
-                        $('#pessoaResult').html('Pessoa encontrada: ' + response.nome);
-                    } else {
-                        $('#pessoaResult').html('Pessoa não encontrada.');
+    $(document).ready(function() {
+        $('#pessoaInput').on('input', function() {
+            var cpf = $(this).val();
+            if (cpf.length === 11) { // Assuming CPF has 11 digits
+                $.ajax({
+                    url: '/api/pessoafisica/' + cpf,
+                    type: 'GET',
+                    success: function(response) {
+                        if (response && response.nome) { // Verifica se a resposta não está vazia e se possui o nome
+                            $('#pessoaResult').html('Pessoa encontrada: ' + response.nome);
+                        } else {
+                            $('#pessoaResult').html('Pessoa não encontrada.');
+                        }
+                    },
+                    error: function() {
+                        $('#pessoaResult').html('Erro ao buscar pessoa.');
                     }
-                },
-                error: function() {
-                    $('#pessoaResult').html('Erro ao buscar pessoa.');
-                }
-            });
-        } else {
-            $('#pessoaResult').html('');
-        }
+                });
+            } else {
+                $('#pessoaResult').html('');
+            }
+        });
     });
-});
-
-
 
