bbbd1f12ad480abd552363aac1bc4d62ecfb8df7
LuisGustavo377 <luuis.gustavo.andrade7@gmail.com>
2024-02-23 09:35:22 -0300

    Merge WorkLuis in Main

--- application/resources/views/admin/grupo-de-negocios/show.blade.php
+++ application/resources/views/admin/grupo-de-negocios/show.blade.php
@@ -25,42 +25,75 @@
                             <label class="form-label" style="color: #13deb9">Dados Cadastrais</label>
                         </div>
 
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="mb-3 col-md-6">
-                                        <label id="nomeLabel" class="form-label">Nome</label>
-                                        <input type="text" class="form-control @error('name') is-invalid @enderror"
-                                            id="nomeInput" name="name" placeholder="Nome" value="{{ $grupo->nome }}"
-                                            disabled>
-                                        @error('name')
-                                            <div class="invalid-feedback">
-                                                {{ $message }}
-                                            </div>
-                                        @enderror
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="mb-3 col-md-6">
+                                    <label id="nomeLabel" class="form-label">Nome</label>
+                                    <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                        id="nomeInput" name="name" placeholder="Nome" value="{{ $grupo->nome }}"
+                                        disabled>
+                                    @error('name')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
                                     </div>
-                                    <div class="mb-3 col-md-6">
-                                        <label id="statusLabel" class="form-label">Status</label>
-                                        <input type="text" class="form-control @error('status') is-invalid @enderror"
-                                            id="statusInput" name="status" value="{{ ucfirst($grupo->status) }}"
-                                            disabled>
-                                        @error('status')
-                                            <div class="invalid-feedback">
-                                                {{ $message }}
-                                            </div>
-                                        @enderror
-                                    </div>
+                                    @enderror
                                 </div>
-                                <div class="mb-3">
-                                    <label id="observacaoLabel" class="form-label">Observações</label>
-                                    <textarea class="form-control @error('observacao') is-invalid @enderror" id="observacaoInput" name="observacao"
-                                        disabled>{{ $grupo->observacao }}</textarea>
-                                    @error('observacao')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
+                                <div class="mb-3 col-md-6">
+                                    <label id="statusLabel" class="form-label">Status</label>
+                                    <input type="text" class="form-control @error('status') is-invalid @enderror"
+                                        id="statusInput" name="status" value="{{ ucfirst($grupo->status) }}" disabled>
+                                    @error('status')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
                                     @enderror
                                 </div>
                             </div>
+
+                            <div class="row">
+                                <div class="mb-3 col-md-12">
+                                    <label id="unidadesLabel" class="form-label">Unidades Vinculadas:</label>
+
+                                    <!-- Se houver unidades de negócio vinculadas -->
+
+                                    <label class="form-control">
+                                        @if ($unidades->count() > 0)
+                                        
+                                        <ul>
+                                            @foreach ($unidades as $unidade)
+                                            @if ($unidade->tipo_pessoa == 'pf')
+                                            <!-- Se for pessoa física -->
+                                            <li><a
+                                                    href="{{ route('admin.pessoa-fisica.show', $unidade->pessoaFisica->id) }}">{{ $unidade->pessoaFisica->nome }}</a>
+                                            </li>
+                                            @elseif ($unidade->tipo_pessoa == 'pj')
+                                            <!-- Se for pessoa jurídica -->
+                                            <li><a
+                                                    href="{{ route('admin.pessoa-juridica.show', $unidade->pessoaJuridica->id) }}">{{ $unidade->pessoaJuridica->nome }}</a>
+                                            </li>
+                                            @endif
+                                            @endforeach
+                                        </ul>
+                                        @else
+                                        <!-- Se não houver unidades de negócio vinculadas -->
+                                        <p>Ainda não há nenhuma unidade vinculada a este grupo de negócio.</p>
+                                        @endif
+                                    </label>
+                                </div>
+                            </div>
+
+
+                            <div class="mb-3">
+                                <label id="observacaoLabel" class="form-label">Observações</label>
+                                <textarea class="form-control @error('observacao') is-invalid @enderror"
+                                    id="observacaoInput" name="observacao" disabled>{{ $grupo->observacao }}</textarea>
+                                @error('observacao')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="mb-3 d-flex justify-content-end">
                             <div class="mb-3">
