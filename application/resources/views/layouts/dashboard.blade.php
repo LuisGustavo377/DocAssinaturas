93403fe65ae8f64e564e91189d238c81797c0ccd
leeticiafgs <leticia_fernanda93@gmail.com>
2024-03-05 15:36:56 -0300

    Ajustes Finais

--- application/resources/views/layouts/dashboard.blade.php
+++ application/resources/views/layouts/dashboard.blade.php
@@ -46,8 +46,7 @@
     <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/js/sidebarmenu.js') }}"></script>
     <script src="{{ asset('assets/js/app.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('assets/libs/simplebar/dist/simplebar.js') }}"></script>
+    <
     <script src="{{ asset('assets/js/dashboard.js') }}"></script>
     <script src="{{ asset('assets/js/funcoes.js') }}"></script>
     <script src="{{ asset('assets/js/alertas.js') }}"></script>
